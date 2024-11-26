7a40369d75ead4df172202c64cd6780bf9b92eb7
dhananjay-yadav-it <dhananjayyadav1807@gmail.com>
2024-11-26 13:51:53 +0530

    host on localhost

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,12 @@
 const conectTomongo = require("./Db");
 const express = require("express");
 var cors = require('cors')
-require('dotenv').config();
+// require('dotenv').config();
 conectTomongo();
 
 const app = express();
 const port = 5000;
-const listenUri = process.env.LISTEN_URI ;
+// const listenUri = process.env.LISTEN_URI ;
 
 app.use(cors());
 app.use(express.json());
@@ -14,10 +14,10 @@ app.use(express.json());
 app.use("/api/auth/", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-// app.listen(port, () => {
-//   console.log(authSecreatekey)
-//   console.log(dbUrl)
-//   console.log(`inotebook app listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(authSecreatekey)
+  console.log(dbUrl)
+  console.log(`inotebook app listening on port ${port}`);
+});
 
-app.listen(port, listenUri);
+// app.listen(port, listenUri);
