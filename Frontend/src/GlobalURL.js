c9913cb179ed53286b476679ea64e6ec961e9620
dhananjay-yadav-it <dhananjayyadav1807@gmail.com>
2024-11-26 14:40:07 +0530

    coreect env url after base url

--- Frontend/src/GlobalURL.js
+++ Frontend/src/GlobalURL.js
@@ -1,10 +1,10 @@
 
 export const BASEURL = process.env.REACT_APP_API_KEY
-export const Register_URL = `${BASEURL}/api/auth/createuser`
-export const LOGIN_URL = `${BASEURL}/api/auth/login`
-export const GETUSER_URL = `${BASEURL}/api/auth/getuser`
+export const Register_URL = `${BASEURL}api/auth/createuser`
+export const LOGIN_URL = `${BASEURL}api/auth/login`
+export const GETUSER_URL = `${BASEURL}api/auth/getuser`
 
-export const GETALLNOTES_URL = `${BASEURL}/api/notes/fetchallnotes`
-export const ADDNOTE_URL = `${BASEURL}/api/notes/addnote`
-export const UPDATENOTE_URL = `${BASEURL}/api/notes/updatenote`
-export const DELETENOTE_URL = `${BASEURL}/api/notes/deletenote`
+export const GETALLNOTES_URL = `${BASEURL}api/notes/fetchallnotes`
+export const ADDNOTE_URL = `${BASEURL}api/notes/addnote`
+export const UPDATENOTE_URL = `${BASEURL}api/notes/updatenote`
+export const DELETENOTE_URL = `${BASEURL}api/notes/deletenote`
